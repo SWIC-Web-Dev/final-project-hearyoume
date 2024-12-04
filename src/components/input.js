b2b70b3b09a7de326a07e8f47ae88a574cff3cce
Amanda Ellis <amanda.ellis002@student.swic.edu>
2024-12-04 17:59:02 -0600

    Add a component function for the inputs

---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+export default function Input({id, placeholder, additionalClasses = ""}) {
+  return `
+  <input 
+    id="${id}" 
+    name="${id}"
+    class="w-full px-3 py-2 border rounded shadow-sm ${additionalClasses}" 
+    type="text" 
+    placeholder="${placeholder}"
+    required
+  />
+`;
+}
