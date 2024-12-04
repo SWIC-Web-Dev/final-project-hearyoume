09a03c3d4755347acfcffae8b25abeb470b4183d
Amanda Ellis <amanda.ellis002@student.swic.edu>
2024-12-04 17:59:46 -0600

    Add a component function for a form

---
+++ src/components/form.js
@@ -0,0 +1,32 @@
+import Button from "./button";
+import Input from "./input";
+
+export default function BookForm() {
+  const formFields = [
+    { id: "title", placeholder: "Book title" },
+    { id: "author", placeholder: "Author" },
+    { id: "genre", placeholder: "Genre" },
+    { id: "cost", placeholder: "Cost" },
+  ];
+
+  return `
+    <div class="max-w-4xl mx-auto p-4">
+      <form id="bookForm" class="space-y-4 mb-8">
+        ${formFields
+          .map((field) =>
+            Input({
+              ...field,
+              additionalClasses: "mb-2",
+            }),
+          )
+          .join("")}
+        ${Button({
+          type: "submit",
+          additionalClasses:
+            "w-full bg-indigo-500 text-white px-4 py-2 hover:bg-indigo-600",
+          text: "Add Book",
+        })}
+      </form>
+    </div>
+  `;
+}
