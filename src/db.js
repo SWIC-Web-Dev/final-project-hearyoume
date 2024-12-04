e60ca0aa621a42686052a20bdc0f4bb1e29af418
Amanda Ellis <amanda.ellis002@student.swic.edu>
2024-12-04 15:55:01 -0600

    Create dataset of books

---
+++ src/db.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        title: "Iron Flame",
+        author: "Rebecca Yarros",
+        genre: "Fantasy",
+        cost: "$15.99"
+    },
+    {
+        title: "A Court of Thorns and Roses",
+        author: "Sarah J. Maas",
+        genre: "Fantasy",
+        cost: "$12.99"
+    },
+    {
+        title: "Twilight",
+        author: "Stephenie Meyer",
+        genre: "Fantasy",
+        cost: "$10.99"
+    },
+    {
+        title: "Bird by Bird",
+        author: "Anne Lamott",
+        genre: "Non-Fiction",
+        cost: "$14.99"
+    },
+    { title: "On Writing", 
+      author: "Stephen King", 
+      genre: "Memoir/Writing", 
+      cost: "$16.99"
+    },
+    {
+        title: "Writing Down the Bones",
+        author: "Natalie Goldberg",
+        genre: "Non-Fiction",
+        cost: "$11.99"
+    },
+];
