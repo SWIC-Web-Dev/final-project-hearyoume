0aa1a891f19f2d32564b2b55993326e9f0244cfd
Amanda Ellis <amanda.ellis002@student.swic.edu>
2024-12-04 18:15:28 -0600

    Add the code that imports functions and renders the form and table

--- src/main.js
+++ src/main.js
@@ -1,1 +1,43 @@
-// TODO: Do what must be done
+import BookTable from "./components/book-table/table";
+import BookForm from "./components/form";
+import initialBooks from "./db";
+import state from "./state";
+
+const app = document.querySelector("#app");
+
+state.setState({ books: initialBooks });
+
+function render() {
+  const { books } = state.getState();
+
+  app.innerHTML = `
+    ${BookForm()}
+    ${BookTable(books)}
+  `;
+}
+
+state.subscribe(render);
+render();
+
+document.addEventListener("click", (event) => {
+  const row = event.target.closest("tr");
+  if (row) {
+    state.deleteBook(row.dataset.book);
+  }
+});
+
+document.addEventListener("submit", (event) => {
+  if (event.target.id === "bookForm") {
+    event.preventDefault();
+    const form = event.target;
+
+    state.addBook({
+      title: form.title.value,
+      author: form.author.value,
+      genre: form.genre.value,
+      cost: form.cost.value,
+    });
+
+    form.reset();
+  }
+});
