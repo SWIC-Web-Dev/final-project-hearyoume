28672506fb809f20ffa88fe7f6789585532663de
Amanda Ellis <amanda.ellis002@student.swic.edu>
2024-12-04 18:13:46 -0600

    Add dataset

--- src/db.js
+++ src/db.js
@@ -1,37 +1,44 @@
 export default [
-    {
-        title: "Iron Flame",
-        author: "Rebecca Yarros",
-        genre: "Fantasy",
-        cost: "$15.99"
-    },
-    {
-        title: "A Court of Thorns and Roses",
-        author: "Sarah J. Maas",
-        genre: "Fantasy",
-        cost: "$12.99"
-    },
-    {
-        title: "Twilight",
-        author: "Stephenie Meyer",
-        genre: "Fantasy",
-        cost: "$10.99"
-    },
-    {
-        title: "Bird by Bird",
-        author: "Anne Lamott",
-        genre: "Non-Fiction",
-        cost: "$14.99"
-    },
-    { title: "On Writing", 
-      author: "Stephen King", 
-      genre: "Memoir/Writing", 
-      cost: "$16.99"
-    },
-    {
-        title: "Writing Down the Bones",
-        author: "Natalie Goldberg",
-        genre: "Non-Fiction",
-        cost: "$11.99"
-    },
+  {
+    id: 1,
+    title: "Iron Flame",
+    author: "Rebecca Yarros",
+    genre: "Fantasy",
+    cost: "$15.99",
+  },
+  {
+    id: 2,
+    title: "A Court of Thorns and Roses",
+    author: "Sarah J. Maas",
+    genre: "Fantasy",
+    cost: "$12.99",
+  },
+  {
+    id: 3,
+    title: "Twilight",
+    author: "Stephenie Meyer",
+    genre: "Fantasy",
+    cost: "$10.99",
+  },
+  {
+    id: 4,
+    title: "Bird by Bird",
+    author: "Anne Lamott",
+    genre: "Non-Fiction",
+    cost: "$14.99",
+  },
+  {
+    id: 5,
+    title: "On Writing",
+    author: "Stephen King",
+    genre: "Memoir/Writing",
+    cost: "$16.99",
+  },
+  {
+    id: 6,
+    title: "Writing Down the Bones",
+    author: "Natalie Goldberg",
+    genre: "Non-Fiction",
+    cost: "$11.99",
+  },
 ];
